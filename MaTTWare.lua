9396b65baa70de3a142e73d05f485fb2d78d6985
MaTTox23 <90100670+MaTTox23@users.noreply.github.com>
2022-08-17 00:41:19 +0300

    Update MaTTWare.lua

--- MaTTWare.lua
+++ MaTTWare.lua
@@ -60,8 +60,8 @@ local themeStyles = {
     DarkTheme = {
         SchemeColor = Color3.fromRGB(12, 12, 138),
         Background = Color3.fromRGB(21, 21, 38),
-        Header = Color3.fromRGB(255, 17, 0),
-        TextColor = Color3.fromRGB(255, 17, 0),
+        Header = Color3.fromRGB(20, 20, 38),
+        TextColor = Color3.fromRGB(0, 0, 0),
         ElementColor = Color3.fromRGB(12, 12, 138)
     },
     LightTheme = {
